f4870ad0c027bed3033e63fd42fffe153f269e0d
Joan <setoba1192@hotmail.com>
2023-01-07 17:55:50 -0500

    Junit test for updateEmployee when employee not found REST API

--- spring-boot-testing/src/test/java/net/javaguides/springboot/controller/EmployeeControllerTest.java
+++ spring-boot-testing/src/test/java/net/javaguides/springboot/controller/EmployeeControllerTest.java
@@ -186,4 +186,35 @@ public class EmployeeControllerTest {
                 .andExpect(jsonPath("$.lastName", is(updatedEmployee.getLastName())))
                 .andExpect(jsonPath("$.email", is(updatedEmployee.getEmail())));
     }
+
+    @DisplayName("Junit test for updateEmployee when employee not found REST API")
+    @Test
+    public void givenNotFoundEmployee_whenUpdateEmployee_thenReturnNotFound() throws Exception {
+
+        //given - precondition or setup
+        long employeeId = 1L;
+        Employee savedEmployee = Employee.builder()
+                .firstName("Joan")
+                .lastName("Roa")
+                .email("setoba1192@gmail.com")
+                .build();
+
+        Employee updatedEmployee = Employee.builder()
+                .firstName("Joan Sebastian")
+                .lastName("Roa Sanchez")
+                .email("setoba1192@hotmail.com")
+                .build();
+
+        given(employeeService.getEmployeeById(employeeId)).willReturn(Optional.empty());
+        given(employeeService.updateEmployee(any(Employee.class))).willReturn(updatedEmployee);
+
+        //when - action or the behavior that we are goint to test
+        ResultActions response = mockMvc.perform(put("/api/employees/{id}", employeeId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updatedEmployee)));
+
+        //then - verify the output
+        response.andExpect(status().isNotFound())
+                .andDo(print());
+    }
 }
