c8a11f0e2747b8d408a1b36db1ba5cc2a3c86f9e
Joan <setoba1192@hotmail.com>
2023-01-08 23:03:15 -0500

    Junit integration test for deleteEmployee REST API

--- spring-boot-testing/src/test/java/net/javaguides/springboot/integration/EmployeeControllerITTest.java
+++ spring-boot-testing/src/test/java/net/javaguides/springboot/integration/EmployeeControllerITTest.java
@@ -16,16 +16,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Transactional
@@ -218,4 +214,26 @@ public class EmployeeControllerITTest {
                 .andDo(print());
     }
 
+    // Junit test for deleteEmployee REST API
+    @DisplayName("Junit integration test for deleteEmployee REST API")
+    @Test
+    public void givenEmployeeId_whenDeleteEmployee_thenReturn200Status() throws Exception {
+
+        //given - precondition or setup
+        Employee employee = Employee.builder()
+                .firstName("Joan")
+                .lastName("Roa")
+                .email("setoba1192@gmail.com")
+                .build();
+        employeeRepository.save(employee);
+
+        //when - action or the behavior that we are goint to test
+        ResultActions response = mockMvc.perform(delete("/api/employees/{id}", employee.getId()));
+
+        //then - verify the output
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().string("Employee deleted successfully"));
+
+    }
 }
